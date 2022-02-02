673ea3155fff305cc19af086059dd25c8e2a8af7
GloryEagle49 <onyeglory2019@gmail.com>
2022-02-02 17:30:44 +0100

    Added Fontawesome Library and Started Website Structre

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 My First Repository
 
 This is favour test repository
+
+#  (Font-Awesome Library https://fontawesome.com/v5.15/icons?d=gallery) ['https://fontawesome.com/v5.15/icons?d=gallery]
+
+#  (Bootstrap 5 https://getbootstrap.com/docs/4.6/getting-started/introduction) ['https://getbootstrap.com/docs/4.6/getting-started/introduction/]
---
+++ about.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Project Hub 2</title>
+    <link rel="stylesheet" href="fa/css/all.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <!-- Bootstrap CDN  -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
+    
+    <!-- Tailwind CSS CDN -->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <?php include_once 'components/header.html'; ?>
+    
+    
+
+    <script src="js/script.js"></script>
+</body>
+</html>
