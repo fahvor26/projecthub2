a4bbd6ebd8cb558bec87182e70658c9c13cab2e5
destinybravos <destinybravos@gmail.com>
2022-02-03 01:15:14 +0100

    Added Tailwindcss library to the project

--- index.php
+++ index.php
@@ -6,12 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Project Hub 2</title>
     <link rel="stylesheet" href="fa/css/all.css">
+    <link rel="stylesheet" href="css/tailwind.css">
     <link rel="stylesheet" href="css/styles.css">
     <!-- Bootstrap CDN  -->
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
     
     <!-- Tailwind CSS CDN -->
-    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
 </head>
 <body>
     <?php include_once 'components/header.html'; ?>
