76f369b5809733f25715cde4c4c6805bd89e13a8
Ptechsoft <96241333+Ptechsoft@users.noreply.github.com>
2022-02-04 01:52:51 +0100

    Created contact pag

--- contact.php
+++ contact.php
@@ -14,28 +14,71 @@
     <!-- Tailwind CSS CDN -->
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
+<body class="bg-[#55ffe5]">
     <?php include_once 'components/header.html'; ?>
-    
+    <div class="my-10 mx-20">
+       <h1 class="font-serif text-2xl font-bold">CONNECT WITH US</h1>
+      <p class=>We Would Love to Respond to your queries and help you succeed. Feel Free to get in touch with Us.
+</div>
     <div class="grid grid-cols-1 md:grid-cols-2">
-        <div class="">
-            <div class="my-6 mx-6">
+
+        <div class="flex">
+            <div class="pt-10 pb-5 pl-10 pr-40 ml-40 my-6 bg-[#fff]">
+                <h2 class="mb-10 h-10 font-times font-bold text-left text-2xl">Send Your Request</h2>
                 <form action="backend/test.php" method="post" name="frm_contact">
-                    <div class="mb-3">
-                        <input type="text" name="uname" id="" class="border-black border">
+                    <div class="flex mb-3">
+                        <label class="w-20">Name</label>
+                        <input type="text" placeholder="Sarah Obi" name="uname" id="" class="mb-3 h-10 border-black  border-b-2 outline-none">
                     </div>
 
-                    <div class="mb-3">
-                        <input type="password" name="pass" id="" class="border-black border">
+                    <div class="flex mb-3">
+                        <label class="w-20">Phone</label>
+                        <input type="tel" placeholder="+234 7046383233" name="pass" id="" class="mb-3 h-10 border-black border-b-2 outline-none">
                     </div>
 
-                    <button type="submit" class="bg-blue-500 py-1 px-5 rounded-lg text-white">
-                        Send
-                    </button>
-                    <button type="reset" class="bg-red-500 py-1 px-5 rounded-lg text-white">
-                        Clear
-                    </button>
+                    <div class="flex mb-3">
+                        <label class="w-20">Email</label>
+                        <input type="email" placeholder="emma23@gmail.com" name="email" id="" class="mb-3 h-10 border-black border-b-2 outline-none">
+                    </div>
+
+                    <div class="flex mb-3">
+                        <label class="w-20">Subject</label>
+                        <input type="text" placeholder="Product demo"  name="subject" id="" class="mb-10 h-10 border-black border-b-2 outline-none"> 
+                    </div>
+                    <div class="mb-3">
+                            <label class="w-20 ">Message</label>
+                            <textarea rows="5" placeholder="Your Message" class="mt-10 border-black border-2 rounded-xl pr-40" ></textarea>
+                    </div>
+                    <div class="text-left">
+                        <button type="submit" class="bg-blue-500 py-1 px-5 rounded-lg text-white">
+                            Send
+                        </button>
+                        <button type="reset" class="bg-red-500 py-1 px-5 rounded-lg text-white">
+                            Clear
+                        </button>
+                    </div>
                 </form>
+            </div>
+
+            <div class=" text-white pt-10 pb-5 pl-10 pr-40 my-6 bg-[#1c00b5]">
+                <h3 class="w-60 font-times font-bold text-white text-2xl">Reach Us</h3>
+                <table>
+                    <tr class="pr-20 pt-20">
+                        <td class="pr-20 pt-20">Email</td>
+                        <td class="pr-20 pt-20">creativeconcept@gmail.com</td>
+                    </tr>
+
+                    <tr class="pr-20 pt-10">
+                        <td class="pr-20 pt-10">Phone</td>
+                        <td class="pr-20 pt-10">+234 7083458457</td>
+                    </tr>
+
+                    <tr class="pr-20 pt-10">
+                        <td class="pr-20 pt-10">Address</td>
+                        <td class="pr-20 pt-10">Procity, Federal Polytechnic<br> Nekede, Owerri, Imo State, Nigeria.</td>
+                    </tr>
+
+                </table>    
             </div>
         </div>
         <!-- Second Column -->
