bc9e0de24e9a13643bdd619b590a9e48bd5847b7
Ptechsoft <96241333+Ptechsoft@users.noreply.github.com>
2022-02-08 15:56:31 +0100

    DId little change

--- about.php
+++ about.php
@@ -16,9 +16,35 @@
 </head>
 <body>
     <?php include_once 'components/header.html'; ?>
-    
-    
+    <div class="mt-10 py-8 bg-blue-500 w-50 h-40 text-3xl text-center font-serif">
+        About Us
+        
+    </div>
 
+    <div class="mt-8 mx-10 py-5 bg-blue-500 text-black w-50 h-60 text-2xl text-center font-serif"> 
+        A NEW CREATIVITY WEB PLATFORM <BR>
+        This page is all about been creative, learn how to grow ur confidence<br>
+        and create things around you, you will enjoy our services and then thank us later <br>
+        we care about your potential
+    </div>
+
+    <div class="text-center mt-5">
+        <button class="bg-blue-700 text-2xl text-center px-6 py-3 font-serif rounded-2xl ">
+            Learn More
+        </button>
+    </div>
+
+
+        <!-- <form class="inline-block" >
+            <div class="grid">
+                <label class="ml-10">Phone</label>
+                <input type="tel" placeholder="+234 7046383233" name="phone" id="phone" class="ml-10 ">
+            </div>
+            <div class="grid">
+                <label class="ml-10">Email</label>
+                <input type="email" placeholder="fahvor@gmail.com" name="email" id="email" class="ml-10 ">
+            </div>
+        </form> -->
     <script src="js/script.js"></script>
 </body>
 </html>
